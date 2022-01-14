8ead8f40ce626aa110f67df7a9a73a435357f609
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-14 17:19:26 -0300

    adicionando menu ao main do projeto

--- main.rb
+++ main.rb
@@ -1,6 +1,22 @@
 require_relative './lib/tree/tree_avl'
 require_relative './lib/git/git'
 
-git = Git.new('/var/www/study/github/ruby_on_rails/locallibrary', AVLTree.new)
+def messages
+  puts '================= Bem vindo ================'
+  puts '1 - Analisar o arquivo localmente'
+  puts '2 - Baixar um repositório'
+end
 
-git.print_all_contribuitores
+def main
+  messages
+  choice = gets.chomp.to_i
+  if choice == 1
+    puts 'Informe o caminho do projeto'
+    path = gets.chomp
+    Git.new(path, AVLTree.new).print_all_contribuitores
+  else
+    puts 'Em desenvolvimento'
+  end
+end
+
+main
